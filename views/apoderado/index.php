d1361e3200e9bb34c4fb02cbd39e5befe7239ff4
angelPalominos <palominosangel71@gmail.com>
2023-06-19 11:18:21 -0400

    Vista Apoderado

---
+++ views/apoderado/index.php
@@ -0,0 +1,316 @@
+<main>
+    <header class="encabezado">
+        <div class="title"> <img src="" alt="imagen bien fachera"></div>
+        <a href="" class="boton">Logout</a>
+    </header>
+    <nav class="cont nav">
+        <p id="b3 boton" id="Apoderado">Atraso</p>
+    </nav>
+    <table class="table">
+        <h2 class="tab-title">Nombre alumno</h2>
+        <thead class="Atraso b1">
+            <tr>
+                <th>ID</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Rut Estudiante</th>
+                <th>Rut Funncionario</th>
+            </tr>
+
+        </thead>
+        <tbody>
+            <?php foreach($atrasos as $atraso): ?>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <table class="table">
+        <h2 class="tab-title">Nombre alumno</h2>
+        <thead class="Atraso b1">
+            <tr>
+                <th>ID</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Rut Estudiante</th>
+                <th>Rut Funncionario</th>
+            </tr>
+
+        </thead>
+        <tbody>
+            <?php foreach($atrasos as $atraso): ?>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $atraso->id_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->fecha_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->hora_atraso ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_estudiante ?>
+                </td>
+                <td>
+                    <?php echo $atraso->rut_func ?>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</main>
